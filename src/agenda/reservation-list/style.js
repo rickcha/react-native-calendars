213eeb7b046bc9a91b8a93b33a32e637f37a9b03
Rick Cha <rick.cha@outlook.com>
2019-01-05 20:07:22 -0800

    changing orientation of the agenda view

--- src/agenda/reservation-list/style.js
+++ src/agenda/reservation-list/style.js
@@ -1,13 +1,13 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 import * as defaultStyle from '../../style';
 
 const STYLESHEET_ID = 'stylesheet.agenda.list';
 
 export default function styleConstructor(theme = {}) {
-  const appStyle = {...defaultStyle, ...theme};
-  return  StyleSheet.create({
+  const appStyle = { ...defaultStyle, ...theme };
+  return StyleSheet.create({
     container: {
-      flexDirection: 'row'
+      flexDirection: 'column'
     },
     dayNum: {
       fontSize: 28,
@@ -22,10 +22,10 @@ export default function styleConstructor(theme = {}) {
       backgroundColor: 'rgba(0,0,0,0)'
     },
     day: {
-      width: 63,
+      flex: 1,
       alignItems: 'center',
-      justifyContent: 'flex-start',
-      marginTop: 32
+      justifyContent: 'center',
+      marginVertical: 32
     },
     today: {
       color: appStyle.agendaTodayColor
